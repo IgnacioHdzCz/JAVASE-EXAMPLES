b0d250564b3d71291efaefdb0b5c2f294d8c13f5
ignaciohdzcz <ignacio.hdz.cz@gmail.com>
2023-01-17 10:54:23 -0600

    Saludo basico con clase utils

---
+++ App1/src/com/java/app/App.java
@@ -0,0 +1,14 @@
+package com.java.app;
+
+import com.java.app.utils.UtilsApp;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UtilsApp utilsApp = new UtilsApp();
+		utilsApp.saludo();
+		
+	}
+
+}
---
+++ App1/src/com/java/app/utils/UtilsApp.java
@@ -0,0 +1,8 @@
+package com.java.app.utils;
+
+public class UtilsApp {
+
+	public void saludo() {
+		System.out.println("Hello!!");
+	}
+}
