9871ea120eed7fc60f0ece80133db7f35b907af5
ignaciohdzcz <ignacio.hdz.cz@gmail.com>
2023-01-17 12:06:59 -0600

    Tipo de Variables

--- App1/src/com/java/app/App.java
+++ App1/src/com/java/app/App.java
@@ -9,6 +9,42 @@ public class App {
 		UtilsApp utilsApp = new UtilsApp();
 		utilsApp.saludo();
 		
+		/*
+		 * Concepto de Variables
+		 * int  : son numeros enteros positivos y negativos
+		 * float : hace referencia a valores flotantes : decimales : 1.2 , 4.6, 9.7
+		 * double : valores decimales : guarda mas números 1.2 ...
+		 * String : cadenas : Hola mi nombre es Christian
+		 * char : character : solo letras
+		 * boolean : TRUE o False
+		 */
+		
+		// Primera variable entera
+		
+		int numero = 19;
+		
+		System.out.println(numero);
+		//Creamos nuestras primeras varianles en flotantes y double
+		// Cuando se crea un numero flotante se pone una f
+		float numeroFloat = 2.09f;
+		double numeroDouble = 9.7;
+		
+		System.out.println("Numero float " + numeroFloat);
+		System.out.println("Numero double " + numeroDouble);
+				
+		
+		//Variables char y String
+		String texto = "Hola me llamo Ignacio";
+		texto += " soy un String";
+		char caracter = 'K';
+		
+		System.out.println("Cadena de caracter " + texto);
+		System.out.println("Caracter " + caracter);
+		//Variable boolean
+		boolean variable_boolean = true;
+		System.out.println(variable_boolean);
+		
+		
 	}
 
 }
